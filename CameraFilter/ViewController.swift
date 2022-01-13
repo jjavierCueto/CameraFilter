8d1c7666b77fc3ae348e94a17fa1a0f49863aa6b
Javier Cueto <jose.cueto@wizeline.com>
2022-01-13 09:15:36 -0700

    Initial Commit

---
+++ CameraFilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Javier Cueto on 13/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
